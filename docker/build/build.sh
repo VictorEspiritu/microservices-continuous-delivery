8390f85995dcc3dc33d998d06ce3438dd89fdcce
Matthias Noback <matthiasnoback@gmail.com>
2017-03-30 10:59:54 +0200

    Some more syntax fixes

--- docker/build/build.sh
+++ docker/build/build.sh
@@ -4,15 +4,15 @@ set -e
 
 BUILD_DIRECTORY=/opt
 
-APP_DIRECTORY=$BUILD_DIRECTORY/app
+APP_DIRECTORY="${BUILD_DIRECTORY}/app"
 # Install non-dev dependencies
 composer install \
     --no-dev \
     --prefer-dist \
     --optimize-autoloader \
     --no-interaction \
-    --working-dir=$APP_DIRECTORY
+    --working-dir="${APP_DIRECTORY}"
 
 # Tar/zip all production files
 BUILD_TAR="$BUILD_DIRECTORY/docker/service/build.tar.gz"
-tar -czf $BUILD_TAR -C $APP_DIRECTORY .
+tar -czf "${BUILD_TAR}" -C "${APP_DIRECTORY}" .
